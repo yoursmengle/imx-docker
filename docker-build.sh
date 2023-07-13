ea70f9c064450a7aef69c48721b5f4bedd723172
yoursmengle <yoursmengle@126.com>
2023-07-13 16:56:26 +0800

    always use ubuntu 20.04

--- docker-build.sh
+++ docker-build.sh
@@ -8,21 +8,10 @@
 # source the common variables
 . ./env.sh
 
-usage() {
-    echo -e "\e[3m\nUsage: $0 [path_to_Dockerfile]\e[0m\n"
-}
-
-# main
-
-if [ $# -ne 1 ]
-    then
-        usage
-    else
-        docker build --tag "${DOCKER_IMAGE_TAG}" \
-                     --build-arg "DOCKER_WORKDIR=${DOCKER_WORKDIR}" \
-                     --build-arg "USER=$(whoami)" \
-                     --build-arg "host_uid=$(id -u)" \
-                     --build-arg "host_gid=$(id -g)" \
-                     -f $1 \
-                     .
-fi
+docker build --tag "${DOCKER_IMAGE_TAG}" \
+             --build-arg "DOCKER_WORKDIR=${DOCKER_WORKDIR}" \
+             --build-arg "USER=$(whoami)" \
+             --build-arg "host_uid=$(id -u)" \
+             --build-arg "host_gid=$(id -g)" \
+             -f Dockerfile-Ubuntu-20.04 \
+             .
