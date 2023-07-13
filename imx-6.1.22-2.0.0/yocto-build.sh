d96f7fd398c849e3f5c4b093d08b3ca571050a4e
yoursmengle <yoursmengle@126.com>
2023-07-13 19:00:25 +0800

    add get repo

--- imx-6.1.22-2.0.0/yocto-build.sh
+++ imx-6.1.22-2.0.0/yocto-build.sh
@@ -4,11 +4,16 @@
 # source the common variables
 
 . imx-6.1.22-2.0.0/env.sh
-cat  imx-6.1.22-2.0.0/env.sh
 #
 
 mkdir -p ${YOCTO_DIR}
 cd ${YOCTO_DIR}
+
+# get repo
+if [ ! -d git-repo ]; then
+    git clone https://gerrit-googlesource.proxy.ustclug.org/git-repo
+fi
+export PATH=${YOCTO_DIR}/git-repo:${PATH}
 
 # Init
 
@@ -22,8 +27,6 @@ repo sync -j`nproc`
 # source the yocto env
 
 EULA=1 MACHINE="${MACHINE}" DISTRO="${DISTRO}" source imx-setup-release.sh -b build_${DISTRO}
-
-echo "MACHINE=${MACHINE}"
 
 # Build
 
